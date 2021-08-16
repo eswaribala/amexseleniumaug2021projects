1b5fb799379a729b86b9268beee7df43ac68d8e8
eswaribala <parameswaribala@gmail.com>
2021-08-16 13:32:52 +0530

    updated

--- canadasitetest/src/main/java/com/amex/utility/PhoneSearchTest.java
+++ canadasitetest/src/main/java/com/amex/utility/PhoneSearchTest.java
@@ -2,7 +2,11 @@ package com.amex.utility;
 
 import java.util.ResourceBundle;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class PhoneSearchTest {	
+	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -10,6 +14,15 @@ public class PhoneSearchTest {
 		ResourceBundle resourceBundle=ResourceBundle.getBundle("canadasearch");
 
 		System.out.println(resourceBundle.getString("webDriverPath"));
+		System.out.println(resourceBundle.getString("baseUrl"));
+		
+		String driverPath=resourceBundle.getString("webDriverPath");
+		String baseUrl=resourceBundle.getString("baseUrl");
+		System.setProperty("webdriver.chrome.driver",driverPath);
+		WebDriver webDriver=new ChromeDriver();
+		webDriver.get(baseUrl);
+		webDriver.close();
+		
 		
 	}
 
