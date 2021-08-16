f401036318a44bc3ac3a0b1ead99cddc9e6b879d
eswaribala <parameswaribala@gmail.com>
2021-08-16 13:07:40 +0530

    updated

---
+++ canadasitetest/src/main/java/com/amex/utility/PhoneSearchTest.java
@@ -0,0 +1,18 @@
+package com.amex.utility;
+
+import java.util.ResourceBundle;
+
+public class PhoneSearchTest {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ResourceBundle resourceBundle=ResourceBundle.getBundle("canadasearch");
+
+		System.out.println(resourceBundle.getString("webDriverPath"));
+		
+	}
+
+}
