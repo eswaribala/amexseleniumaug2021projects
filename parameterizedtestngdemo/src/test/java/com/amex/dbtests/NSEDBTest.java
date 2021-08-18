f24eca11959fc23ef6cff554928835ba14573746
eswaribala <parameswaribala@gmail.com>
2021-08-18 15:44:26 +0530

    Updated

---
+++ parameterizedtestngdemo/src/test/java/com/amex/dbtests/NSEDBTest.java
@@ -0,0 +1,67 @@
+package com.amex.dbtests;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.ResourceBundle;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.amex.csvfiles.models.User;
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+public class NSEDBTest {
+	private WebDriver webDriver;
+	private WebElement element;
+	private String driverPath,nseUrl,fileName;
+	private   ResourceBundle resourceBundle;
+    private Connection conn;     
+	@BeforeTest
+	public void beforeTest() {		
+		resourceBundle=ResourceBundle.getBundle("googlesearch");	
+		driverPath=resourceBundle.getString("webDriverPath");
+		nseUrl=resourceBundle.getString("nseUrl");
+		fileName=resourceBundle.getString("xmlFileName");
+		try {
+			conn=DBHelper.getConnection();
+			if(conn!=null) {
+				log.info("DB Connection ready....");
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			log.info(e.getMessage());
+		}
+		
+	
+		System.setProperty("webdriver.chrome.driver",driverPath);
+		//WebDriverManager.chromedriver().setup();
+		webDriver=new ChromeDriver();			
+	}
+	@Test
+	public void nseReportTest() {
+		webDriver.get(nseUrl);
+		webDriver.manage().window().maximize();
+		WebDriverWait wait=new WebDriverWait(webDriver,10);
+		//wait.until(ExpectedConditions.visibilityOfElementLocated();
+		log.info(webDriver.getTitle());
+		
+	}
+	@AfterTest
+	public void afterTest() {
+		webDriver.close();
+	}
+}
