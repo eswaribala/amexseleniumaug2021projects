ed5a9dd64f5b82f4d19d4a9095036df96313426f
eswaribala <parameswaribala@gmail.com>
2022-01-12 00:40:37 +0530

    updated

--- cucumbermaven/src/test/java/runnerFiles/RunCucumberTest.java
+++ cucumbermaven/src/test/java/runnerFiles/RunCucumberTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = {"pretty","html:target/cucumber-reports","json:target/cucumberJsonReport"},features = {
+@CucumberOptions(plugin = {"pretty","html:target/cucumber-reports","json:target/cucumber.json"},features = {
 		"src/test/resources/featureFiles/login.feature"
 		},glue="com.amex.cucumberdemo")
 public class RunCucumberTest {
