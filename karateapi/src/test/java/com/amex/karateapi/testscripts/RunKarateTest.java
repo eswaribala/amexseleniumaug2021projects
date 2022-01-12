75f8b6bce99b4f655c12d51fec69ef424d33add5
eswaribala <parameswaribala@gmail.com>
2022-01-12 09:17:59 +0530

    updated

--- karateapi/src/test/java/com/amex/karateapi/testscripts/RunKarateTest.java
+++ karateapi/src/test/java/com/amex/karateapi/testscripts/RunKarateTest.java
@@ -3,16 +3,13 @@ import org.junit.runner.RunWith;
 
 import com.intuit.karate.junit4.Karate;
 
+import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 
 
-
-
 @RunWith(Karate.class)
-@CucumberOptions(plugin = {"pretty","html:target/cucumber-reports","json:target/cucumber.json"},features = {
-		"src/test/java/com/amex/karateapi/testscripts/userDetails.feature"
-		},glue="com/amex/karateapi/testscripts")
+
 public class RunKarateTest 
 {
 	
