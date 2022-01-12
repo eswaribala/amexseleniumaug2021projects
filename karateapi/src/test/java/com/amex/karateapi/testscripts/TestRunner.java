691f55f436627539b07a92c6b44ffbb2017a6ce8
eswaribala <parameswaribala@gmail.com>
2022-01-12 08:59:22 +0530

    updated

--- karateapi/src/test/java/com/amex/karateapi/testscripts/TestRunner.java
+++ karateapi/src/test/java/com/amex/karateapi/testscripts/TestRunner.java
@@ -2,9 +2,13 @@ package com.amex.karateapi.testscripts;
 import org.junit.runner.RunWith;
 
 import com.intuit.karate.junit4.Karate;
- 
+
+
+
+
 @RunWith(Karate.class)
+
 public class TestRunner 
 {
- 
+	
 }
