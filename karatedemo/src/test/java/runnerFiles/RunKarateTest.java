9bed9e485214730f5aee9de2adf27467adfa1231
eswaribala <parameswaribala@gmail.com>
2022-01-12 09:27:18 +0530

    updated

---
+++ karatedemo/src/test/java/runnerFiles/RunKarateTest.java
@@ -0,0 +1,16 @@
+package runnerFiles;
+import org.junit.runner.RunWith;
+
+import com.intuit.karate.junit4.Karate;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+
+
+@RunWith(Karate.class)
+
+public class RunKarateTest 
+{
+	
+}
