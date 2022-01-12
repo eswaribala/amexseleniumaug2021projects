8a4a73cc54bd32d300c8bef3ebee0ed221389f94
eswaribala <parameswaribala@gmail.com>
2022-01-12 11:57:17 +0530

    updated

--- canadasitetest/src/test/java/com/amex/tests/PhoneSearchTestNGV1Test.java
+++ canadasitetest/src/test/java/com/amex/tests/PhoneSearchTestNGV1Test.java
@@ -4,10 +4,12 @@ import java.util.ResourceBundle;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PhoneSearchTestNGV1Test {
 
+	private WebDriver webDriver;
 	@Test
 	public void canadaSearchTest() {
 		ResourceBundle resourceBundle=ResourceBundle.getBundle("canadasearch");
@@ -18,10 +20,18 @@ public class PhoneSearchTestNGV1Test {
 		String driverPath=resourceBundle.getString("webDriverPath");
 		String baseUrl=resourceBundle.getString("baseUrl");
 		System.setProperty("webdriver.chrome.driver",driverPath);
-		WebDriver webDriver=new ChromeDriver();
+		webDriver=new ChromeDriver();
 		webDriver.get(baseUrl);
 		System.out.println(webDriver.getTitle());
 		webDriver.close();
 		
 	}
+	
+	
+	  @Test public void canadaSearchTitleTest() {
+	    Assert.assertEquals(webDriver.getTitle(),
+	 "Free People Search, Reverse Phone Lookup, Business Telephone Directory | Canada411.ca"
+	  ); 
+	    }
+	 
 }
